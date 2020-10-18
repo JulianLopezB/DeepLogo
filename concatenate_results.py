5a3fe924795edcdae4b9728a843f3992ea0b907f
julianlopezb <julianlopezba@gmail.com>
2020-10-18 20:52:01 -0300

    concatenation.py

--- concatenate_results.py
+++ concatenate_results.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import argparse
 from src.utils import concatenate_anno
+import pickle
 
 # ARGS 
 
@@ -12,7 +13,12 @@ args = parser.parse_args()
 
 video_category = args.category
 
-output_file = f'{video_category}.csv'
+output_file = f'./data/video-files/{video_category}/{video_category}.csv'
 
-df = concatenate_anno('.data/video-files/', video_category)
+#outfile = open(output_file, "wb")
+df = concatenate_anno('./data/video-files/', video_category).reset_index(drop=True)
+#pickle.dump(df, outfile)
+#outfile.close()
+
 df.to_csv(output_file)
+#df.to_json(output_file)
